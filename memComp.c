e484dad6d5145af94859a0643f64468a3615a7bb
MarioSanchezCap <mario.sanchez-external.garcia@capgemini.com>
2023-04-17 13:26:08 +0200

    subiendo libreria version intermedia

--- memComp.c
+++ memComp.c
@@ -5,16 +5,16 @@
 #include <sys/ipc.h>
 #include <sys/shm.h>
 #include "memComp.h"
-#include "memComp2.h" /* Fichero que contiene la información de la estructura de datos a
+#include "memComp2.h"
+
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/sem.h>
+ /* Fichero que contiene la información de la estructura de datos a
 
 
 compartir */
 /* Función principal */
-
-
-
-#include <semaphore.h>
-
 
 
 int shmid1;
@@ -25,14 +25,10 @@ int currentShmid;
 TAttitude *Attitude;
 TPos *Pos;
 
-TAttitude Attitude1;
-TPos Pos1;
-
-
-
-
-
 int SHM_Init(void){
+
+    int semid = semget(0xa,0,0);
+
     if((shmid1 = shmget(SHM_ID_1, sizeof(TAttitude), IPC_CREAT|0666)) == -1)
 {
 perror("shmget1");
@@ -47,8 +43,10 @@ exit(EXIT_FAILURE);
     Pos = (TPos *)shmat(shmid2,0,0);
 }
 
-int SHM_InitSlot(unsigned int slot_id){
+int SHM_InitSlot(unsigned int slot_id)
+{
     if(slot_id==&shmid1){
+        sem_init(&sem1,1,1);
         if((shmid1 = shmget(SHM_ID_1, sizeof(TAttitude), IPC_CREAT|0666)) == -1)
 {
 perror("shmget1");
@@ -57,6 +55,7 @@ exit(EXIT_FAILURE);
     Attitude = (TAttitude *)shmat(shmid1,0,0);
     }
     if(slot_id==&shmid2){
+            sem_init(&sem2,1,1);
             if((shmid2 = shmget(SHM_ID_2, sizeof(TPos), IPC_CREAT|0666)) == -1)
 {
 perror("shmget2");
@@ -66,44 +65,44 @@ exit(EXIT_FAILURE);
     }
     
     
- 
 }
 
-int  SHM_ReadSlot(unsigned int slot_id, void* data, unsigned int data_size){
-    
-
-    if(slot_id==shmid1){
-        Attitude1.pitch;=Attitude->pitch;
-        Attitude1.roll=Attitude->roll;
-        Attitude1.yaw=Attitude->yaw;
-    }
-    if(slot_id==shmid2){
-    }
-
-    data = shmat(slot_id,0,0);
-
-}
-int  SHM_WriteSlot(unsigned int slot_id, void* data, unsigned int data_size){
-
-}
-
-int main(void)
+int  SHM_ReadSlot(unsigned int slot_id, void* data, unsigned int data_size)
 {
 
-TAttitude *zona;
-/* Creación de la zona de memoria compartida */
+    if(slot_id==shmid1){
+        sem_wait(&sem1);
+        data->pitch=Attitude->pitch;
+        data->roll=Attitude->roll;
+        data->yaw=Attitude->yaw;
+        sem_post(&sem1);
+    }
+    if(slot_id==shmid2){
+        sem_wait(&sem2);
+        data->x=Pos->pitch;
+        data->y=Pos->roll;
+        data->z=Pos->yaw;
+        sem_post(&sem2);
+    }
 
-/* Obtención del puntero a la estructura de datos compartida */
-zona = (TAttitude *)shmat(shmid1,0,0);
-/* Escribimos en la zona de memoria común */
-printf("TAREA es: %f \n", zona->pitch);
-printf("VALOR es: %f \n", zona->roll);
-zona->pitch = 5;
-zona->roll = 16;
-/* Leemos los valores de las variables de la zona de memoria común */
-printf("TAREA es: %d \n", zona->pitch);
-printf("VALOR es: %f \n", zona->roll);
-/* Borrado de la zona de memoria compartida */
-//shmctl(shmid, IPC_RMID,0);
+
 }
-/* Fin función principal */
+int  SHM_WriteSlot(unsigned int slot_id, void* data, unsigned int data_size)
+{
+
+    if(slot_id==shmid1){
+        sem_wait(&sem1);
+        Attitude->pitch=data->pitch;
+        Attitude->roll=data->roll;
+        Attitude->yaw=data->yaw;
+        sem_post(&sem1);
+    }
+    if(slot_id==shmid2){
+        sem_wait(&sem2);
+        Pos->x=data->pitch;
+        Pos->y=data->roll;
+        Pos->z=data->yaw;
+        sem_post(&sem2);
+    }
+
+}
